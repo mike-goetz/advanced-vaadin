c1bf8eb7dd36c7be3039e9909a3c3c196da4eac0
Mike Götz <mike.goetz@ipoint-systems.de>
2017-09-05 16:22:32 +0200

    Initial commit

---
+++ advanced-vaadin-ui/src/main/java/de/wetego/vaadin/config/CookieConfig.java
@@ -0,0 +1,23 @@
+package de.wetego.vaadin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * Cookie Configuration.
+ */
+@Configuration
+public class CookieConfig {
+
+    /**
+     * SessionTrackingConfigListener.
+     *
+     * @return a new instance
+     */
+    @Bean
+    public SessionTrackingConfigListener sessionTrackingConfigListener () {
+        return new SessionTrackingConfigListener ();
+    }
+
+}
